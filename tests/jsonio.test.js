984a4c18d6d602f0f59d0cff7570b175c89b2e84
anoy <kuma.tola3@gmail.com>
2020-12-21 18:51:39 +0900

    refactor: use rootPath

--- tests/jsonio.test.js
+++ tests/jsonio.test.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 const assert = require('assert').strict;
-const jsonioTest = require('../src/utils/jsonio');
+const rootPath = require('app-root-path');
+const jsonioTest = require(`${rootPath}/src/utils/jsonio`);
 
 it('read() and write()', function () {
-    const path = 'tests/jsonio_sample.txt';
+    const path = `${rootPath}/tests/jsonio_sample.txt`;
     const json = JSON.stringify({"sample": [1, 2, 3]});
     jsonioTest.write(path, json);
     assert.equal(jsonioTest.read(path), json);
--- tests/nodejs.test.js
+++ tests/nodejs.test.js
@@ -1,10 +1,9 @@
 const assert = require('assert').strict;
 const fs = require('fs');
-
-const data = JSON.parse(fs.readFileSync('data.json').toString());
+const rootPath = require('app-root-path');
 
 it('readdirSync', function () {
-    const dirs = fs.readdirSync(data["locations"][0]);
+    const dirs = fs.readdirSync(`${rootPath}/tests/sample_dir`);
     assert.equal(dirs.length, 5);
     for (let i = 0; i < dirs.length; i++) {
         assert.equal(dirs[i], `dir0${i + 1}`);
