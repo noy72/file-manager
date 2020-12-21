a96c3566515684cb65606426fa51e02d0019bd65
anoy <kuma.tola3@gmail.com>
2020-12-21 18:48:16 +0900

    chore: rename test files

--- tests/jsonio.test.js
+++ tests/jsonio.test.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 const assert = require('assert').strict;
-const jsonio = require('../src/utils/jsonio');
+const jsonioTest = require('../src/utils/jsonio');
 
 it('read() and write()', function () {
     const path = 'tests/jsonio_sample.txt';
     const json = JSON.stringify({"sample": [1, 2, 3]});
-    jsonio.write(path, json);
-    assert.equal(jsonio.read(path), json);
+    jsonioTest.write(path, json);
+    assert.equal(jsonioTest.read(path), json);
 
     fs.unlinkSync(path);
 });
