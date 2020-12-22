b2b32e152e25df6620b30251aad463ab67e5e978
anoy <kuma.tola3@gmail.com>
2020-12-22 19:22:50 +0900

    fix: remove suffix

--- tests/jsonio.test.js
+++ tests/jsonio.test.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 const assert = require('assert').strict;
 const rootPath = require('app-root-path');
-const jsonioTest = require(`${rootPath}/src/utils/jsonio`);
+const jsonio = require(`${rootPath}/src/utils/jsonio`);
 
 it('read() and write()', function () {
     const path = `${rootPath}/tests/jsonio_sample.txt`;
     const json = JSON.stringify({"sample": [1, 2, 3]});
-    jsonioTest.write(path, json);
-    assert.equal(jsonioTest.read(path), json);
+    jsonio.write(path, json);
+    assert.equal(jsonio.read(path), json);
 
     fs.unlinkSync(path);
 });
