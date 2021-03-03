e2118c1690ee597f9285742a2e29ac40088ce835
anoy <kuma.tola3@gmail.com>
2021-03-04 05:51:34 +0900

    refactor: remove unused the function

--- src/utils/deserialize.ts
+++
@@ -1,8 +0,0 @@
-const deserialize = (field: object, emptyObj: any) => {
-    Object.entries(field).forEach(
-        ([key, value]: [string, object]) => emptyObj[key] = value
-    );
-    return emptyObj;
-};
-
-export default deserialize;
