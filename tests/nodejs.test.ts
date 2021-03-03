2cade2bcfe9c9d1de1d7001b199c0637e7a6ea4e
anoy <kuma.tola3@gmail.com>
2021-03-03 13:31:13 +0900

    chore: remove tests

--- tests/nodejs.test.ts
+++ tests/nodejs.test.ts
@@ -1,14 +1,4 @@
 import * as assert from "assert";
-import * as fs from "fs";
-import { sampleDirPath } from "./testUtils";
-
-it('readdirSync', function () {
-    const dirs = fs.readdirSync(sampleDirPath);
-    assert.strictEqual(dirs.length, 7);
-    for (let i = 1; i < dirs.length; i++) {
-        assert.strictEqual(dirs[i], `dir0${i}`);
-    }
-});
 
 it('instanceof', () => {
     interface Inter {
