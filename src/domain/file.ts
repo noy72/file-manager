8934240e35a835aaccd2fd2905d3efe048198c59
anoy <kuma.tola3@gmail.com>
2021-03-05 15:23:45 +0900

    chore: use node.js functions

--- src/domain/file.ts
+++ src/domain/file.ts
@@ -1,26 +1,26 @@
-import { basename } from "path";
+import { basename, extname } from "path";
 
 const imageFileExts = [
-    "png",
-    "jpg",
-    "jpeg",
+    ".png",
+    ".jpg",
+    ".jpeg",
 ];
 
-const isImageFile = (fileName: string): boolean => imageFileExts.some(ext => fileName.includes(ext));
+const isImageFile = (fileName: string): boolean => imageFileExts.includes(extname(fileName));
 
 const videoFileExts = [
-    "avi",
-    "wmv",
-    "flv",
-    "mpeg",
-    "mp4",
-    "wmv",
-    "webm",
-    "mov"
+    ".avi",
+    ".wmv",
+    ".flv",
+    ".mpeg",
+    ".mp4",
+    ".wmv",
+    ".webm",
+    ".mov"
 ];
 
-const isVideoFile = (fileName: string): boolean => videoFileExts.some(ext => fileName.includes(ext));
+const isVideoFile = (fileName: string): boolean => videoFileExts.includes(extname(fileName));
 
-const isDotFile = (fileName: string): boolean => basename(fileName)[0] === ".";
+const isDotFile = (fileName: string): boolean => basename(fileName).length > 1 && basename(fileName)[0] === ".";
 
 export { isImageFile, isVideoFile, isDotFile };
