cbf16978f597c9dda7a91cb4752f094bef275609
anoy <kuma.tola3@gmail.com>
2021-03-06 17:19:25 +0900

    fix: isValidTagString

--- src/domain/service/tag.ts
+++ src/domain/service/tag.ts
@@ -3,6 +3,11 @@ const parseTagString = (str: string): string[] => {
     return word.length == 2 ? word : ['Prop', word[0]];
 }
 
-const isValidTagString = (str: string): boolean => str.split(":").length <= 2;
+const isValidTagString = (str: string): boolean => {
+    const words = str.split(":");
+    if (words.length == 1) return true;
+    if (words.length == 2 && words.every(word => word.length > 0)) return true;
+    return false;
+}
 
 export { isValidTagString, parseTagString };
