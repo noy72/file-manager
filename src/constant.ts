f0b96309ae42007a9aed8606c17a84fb80914fe0
noy72 <kuma.tola3@gmail.com>
2022-03-13 17:36:13 +0900

    delete

--- src/constant.ts
+++
@@ -1,3 +0,0 @@
-export const RENDER_ITEMS = "render_items";
-export const GET_ITEMS = "get_items";
-export const GET_ITEM = "get_item";
--- src/main/repositories/locationRepository.ts
+++
@@ -1,5 +0,0 @@
-import * as db from '../infrastructure/database';
-
-const getRootLocations = (): string[] => db.getLocations();
-
-export { getRootLocations };
