9539a103306d27717a0ad9455cf060fb3a51bba1
noy72 <kuma.tola3@gmail.com>
2022-03-13 16:05:57 +0900

    chore: add TagList

--- src/renderer/component/tagList.tsx
+++ src/renderer/component/tagList.tsx
@@ -3,12 +3,19 @@ import React, { RefObject } from "react";
 import { Form } from "semantic-ui-react";
 import { Tags } from "../../types";
 
-const TagList = (tags: Tags) =>
-    Object.entries(tags).map(([key, values]) => {
-        return <>
-            <h2>{key}</h2>
-            {values.map(value => <span>{value}</span>)}
-        </>
-    })
+const TagList = ({ tags }: { tags: Tags }) => <>
+    {
+        Object.entries(tags).map(([key, values]) =>
+            <>
+                <h2 key={key}>{key}</h2>
+                {
+                    values.map(value =>
+                        <span key={`${key}-${value}`}>{value}</span>
+                    )
+                }
+            </>
+        )
+    }
+</>
 
 export default TagList;
