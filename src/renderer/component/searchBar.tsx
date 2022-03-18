f5769941c027b36452e821aff10158b1224f2768
noy72 <kuma.tola3@gmail.com>
2022-03-18 15:25:30 +0900

    chore: add icon to SearchBox

--- src/renderer/component/searchBar.tsx
+++ src/renderer/component/searchBar.tsx
@@ -1,5 +1,4 @@
 import React, { RefObject } from "react";
-import { Form } from "semantic-ui-react";
 
 type Event = () => void;
 
@@ -9,18 +8,14 @@ type Props = {
     searchOnClick: Event;
 };
 
+
 const SearchBar = React.forwardRef(
-    (props: Props, ref: RefObject<HTMLInputElement>) => (
-        <Form onSubmit={props.onSubmit} data-testid="form">
-            <input
-                data-testid="input"
-                ref={ref}
-                type="text"
-                placeholder="title #tag"
-            />
-            <i className="times link icon" onClick={props.timesOnClick}></i>
-            <i className="search link icon" onClick={props.searchOnClick}></i>
-        </Form>
+    (props: Props, ref: RefObject<any>) => (
+        <div className="ui big fluid form" onSubmit={props.onSubmit}>
+            <input data-testid="input" ref={ref} type="text" placeholder="title #tag" />
+            <i data-testid="times-icon" className="times link icon" onClick={props.timesOnClick} style={{ marginRight: "2rem" }}></i>
+            <i data-testid="search-icon" className="search link icon" onClick={props.searchOnClick}></i>
+        </div>
     )
 );
 
--- tests/renderer/component/searchBar.test.tsx
+++ tests/renderer/component/searchBar.test.tsx
@@ -4,27 +4,48 @@ import { fireEvent, render } from "@testing-library/react";
 import SearchBar from "../../../src/renderer/component/searchBar";
 
 describe("SearchBar", () => {
-    test("get input value", () => {
+    test("Submit input value", () => {
         const ref = React.createRef<HTMLInputElement>();
-        const mockCallback = jest.fn(() => ref.current.value);
+        const mockOnSubmit = jest.fn(() => ref.current.value);
         const { getByTestId } = render(
             <SearchBar
                 ref={ref}
-                onSubmit={mockCallback}
-                timesOnClick={() => {}}
-                searchOnClick={() => {}}
+                onSubmit={mockOnSubmit}
+                timesOnClick={() => { }}
+                searchOnClick={() => { }}
+            />
+        );
+        const input = getByTestId("input");
+        fireEvent.change(input, { target: { value: "1" } });
+        fireEvent.submit(input);
+        expect(mockOnSubmit.mock.results[0].value).toBe("1");
+
+        fireEvent.change(input, { target: { value: "2" } });
+        fireEvent.submit(input);
+        expect(mockOnSubmit.mock.results[1].value).toBe("2");
+    });
+
+    test("Click icons", () => {
+        const ref = React.createRef<HTMLInputElement>();
+        const mockTimesOnClick = jest.fn();
+        const mockSearchOnClick = jest.fn();
+        const { getByTestId } = render(
+            <SearchBar
+                ref={ref}
+                onSubmit={() => { }}
+                timesOnClick={mockTimesOnClick}
+                searchOnClick={mockSearchOnClick}
             />
         );
 
-        const input = getByTestId("input");
-        fireEvent.change(input, { target: { value: "1" } });
+        const timesIcon = getByTestId("times-icon");
+        expect(mockTimesOnClick.mock.results.length).toBe(0);
+        fireEvent.click(timesIcon);
+        expect(mockTimesOnClick.mock.results.length).toBe(1);
 
-        const form = getByTestId("form");
-        fireEvent.submit(form);
-        expect(mockCallback.mock.results[0].value).toBe("1");
-
-        fireEvent.change(input, { target: { value: "2" } });
-        fireEvent.submit(form);
-        expect(mockCallback.mock.results[1].value).toBe("2");
+        const searchIcon = getByTestId("search-icon");
+        expect(mockSearchOnClick.mock.results.length).toBe(0);
+        fireEvent.click(searchIcon);
+        expect(mockSearchOnClick.mock.results.length).toBe(1);
     });
 });
