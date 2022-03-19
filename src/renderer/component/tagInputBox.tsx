0b3d48f8ab1385931b12bb6fdf925d43760682a2
noy72 <kuma.tola3@gmail.com>
2022-03-19 19:17:25 +0900

    feat: add tag input box

---
+++ src/renderer/component/tagInputBox.tsx
@@ -0,0 +1,36 @@
+import React, { RefObject, forwardRef, FormEvent } from "react";
+import { Button, Form, Input, Select } from "semantic-ui-react";
+import { Tags } from "../../types";
+
+type Props = {
+    tags: Tags,
+    onSubmit: (e: FormEvent<HTMLFormElement>) => void;
+    onChange: (e: FormEvent<HTMLInputElement | HTMLSelectElement>) => void;
+};
+
+const TagInputBox = ({ tags, onSubmit, onChange }: Props) => {
+    const tagGroupOptions = Object.keys(tags).map(key => ({ text: key, value: key }));
+    console.log(tagGroupOptions)
+
+    return (
+        <Form onSubmit={onSubmit} size="tiny">
+            <Form.Group>
+                <Form.Field
+                    control={Select}
+                    onChange={onChange}
+                    options={tagGroupOptions}
+                    placeholder='Tag Group'
+                />
+                <Form.Field
+                    control={Input}
+                    onChange={onChange}
+                    placeholder='Tag'
+                    name="tag"
+                />
+                <Form.Field control={Button} type="submit" inline>Submit</Form.Field>
+            </Form.Group>
+        </Form>
+    )
+}
+
+export default TagInputBox;
--- src/renderer/page/Content.tsx
+++ src/renderer/page/Content.tsx
@@ -1,27 +1,54 @@
-import React, { useEffect, useState } from "react";
+import React, { FormEvent, useEffect, useRef, useState } from "react";
 import "fomantic-ui/dist/semantic.min.css";
 import { useParams } from "react-router-dom";
 import { Container, Grid, Image, List } from 'semantic-ui-react'
-import { ItemForRendererWithGroupedTags, LocalItem } from "../../types";
+import { ItemForRendererWithGroupedTags, LocalItem, Tags } from "../../types";
 import Loading from "../component/Loader";
 import TagList from "../component/tagList";
 import LocalItemList from "../component/localItemList";
+import TagInputBox from "../component/tagInputBox";
+
+
 
 const Content = (): JSX.Element => {
     const { id } = useParams();
     const [item, setItem] = useState(undefined as ItemForRendererWithGroupedTags);
     const [localItems, setLocalItems] = useState([] as LocalItem[]);
+    const [tags, setTags] = useState({} as Tags);
+    const [tagInput, setTagInput] = useState({ tagGroup: "", tag: "" });
 
     useEffect(() => {
         (async () => {
             const item = await window.api.getItem(id);
             setItem(item);
-        })();
-        (async () => {
+
             const localItems = await window.api.getLocalItems(id);
             setLocalItems(localItems);
+
+            const tags = await window.api.getTags();
+            setTags(tags);
         })();
     }, []);
+
+    useEffect(() => {
+        (async () => { })();
+    }, [tags]);
+
+    const tagInputBoxOnSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        console.log(tagInput);
+    };
+
+    const tagInputBoxOnChange = (e: FormEvent<HTMLInputElement | HTMLSelectElement>) => {
+        e.preventDefault();
+
+        const target = e.currentTarget;
+        const name = target.name ? target.name : "tagGroup";
+        const value = target.value ? target.value : target.innerText;
+        setTagInput({ ...tagInput, [name]: value });
+    }
+
 
     return item ?
         <Container>
@@ -30,11 +57,22 @@ const Content = (): JSX.Element => {
                     <Image src={
                         `data:image/${item.thumbnailExt.slice(1)};base64,${item.encodedThumbnail}`
                     } />
+                </Grid.Column>
+                <Grid.Column width={10}>
+                    <TagList tags={item.tags} />
+                </Grid.Column>
+            </Grid>
+            <Grid>
+                <Grid.Column width={6}>
                     <p>{item.createdAt}</p>
                     <p>{item.updatedAt}</p>
                 </Grid.Column>
                 <Grid.Column width={10}>
-                    <TagList tags={item.tags} />
+                    <TagInputBox
+                        tags={tags}
+                        onSubmit={tagInputBoxOnSubmit}
+                        onChange={tagInputBoxOnChange}
+                    />
                 </Grid.Column>
             </Grid>
             <LocalItemList
