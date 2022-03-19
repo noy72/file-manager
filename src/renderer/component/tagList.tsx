2ae529e0f9dbf2233b9b85edb8f1ca5e568af411
noy72 <kuma.tola3@gmail.com>
2022-03-19 09:41:39 +0900

    feat: add link to tags

--- src/renderer/component/tagList.tsx
+++ src/renderer/component/tagList.tsx
@@ -1,15 +1,20 @@
 
 import React from "react";
+import { Label } from "semantic-ui-react";
 import { Tags } from "../../types";
+import { Link } from "react-router-dom";
 
 const TagList = ({ tags }: { tags: Tags }): JSX.Element => <>
     {
         Object.entries(tags).map(([key, values]) =>
             <>
-                <h2 key={key}>{key}</h2>
+                <h3 key={key}>{key}</h3>
                 {
                     values.map(value =>
-                        <span key={`${key}-${value}`}>{value}</span>
+                        <Label key={`${key}-${value}`} as={Link} basic size="small" to={{
+                            pathname: "/",
+                            search: '?search='.concat(`%23"${value}"`),
+                        }}>{value}</Label>
                     )
                 }
             </>
