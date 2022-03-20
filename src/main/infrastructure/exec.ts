1722c6558f785c43fc2591c40f17fc7bbdd315ef
noy72 <kuma.tola3@gmail.com>
2022-03-20 12:25:40 +0900

    chore: remove output for debug

--- src/main/infrastructure/exec.ts
+++ src/main/infrastructure/exec.ts
@@ -6,12 +6,10 @@ export const execCommand = (
     type: ContentType,
     location: string
 ): ChildProcessWithoutNullStreams => {
-    console.log(type, location)
     if (type === 'exe') {
         return spawn(location)
     }
 
     const [command, ...args] = getCommands()[type];
-    console.log(command, args, location)
     return spawn(command, [...args, location]);
 };
